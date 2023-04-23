b15792fa873bf84d901cd0d13a690c9a1c1879dd
TShokhrukh <shohruh8945@gmail.com>
2023-04-23 21:39:53 +0500

    update configuration

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,31 @@
+const path = require('path')
 const CracoAlias = require('craco-alias')
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.module.rules[1].oneOf.forEach((rules) => {
+        if (!rules.test?.toString().match(/sass/)) {
+          return
+        }
+        rules.use.push({
+          loader: 'sass-resources-loader',
+          options: {
+            resources: [
+              path.join(__dirname, 'src', '**', '*.g.scss'),
+              path.join(__dirname, 'src', '**', '*.l.scss')
+            ]
+          }
+        })
+      })
+      return webpackConfig
+    }
+  },
   plugins: [{
     plugin: CracoAlias,
     options: {
       source: 'tsconfig',
-      tsConfigPath: './tsconfig.paths.json'
+      tsConfigPath: path.join(__dirname, 'tsconfig.paths.json')
     }
   }]
 }
