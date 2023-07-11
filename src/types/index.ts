64171b4eeb29461354a018a2fb2b5535fbd2d4df
TShokhrukh <shohruh8945@gmail.com>
2023-07-11 16:39:49 +0500

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,3 @@
-import type { ComponentType } from 'react'
-
-export * from './service.types'
-
-export type TWrapperProps<T extends ComponentType<any>> = T extends ComponentType<infer P> ? P : {}
-
-export interface IModel {
-  onMounted?(): void
-  onUnmounted?(): void
+export type TLocale<T> = {
+  [K: string]: T
 }
