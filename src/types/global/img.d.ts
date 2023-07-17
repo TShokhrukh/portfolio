0b8fd17b1a234e080f58cdb5d55081cef7ab4f71
TShokhrukh <shohruh8945@gmail.com>
2023-07-17 14:59:40 +0500

    add img module

---
+++ src/types/global/img.d.ts
@@ -0,0 +1,23 @@
+declare module '*.gif' {
+  const img: string
+
+  export default img
+}
+
+declare module '*.png' {
+  const img: string
+
+  export default img
+}
+
+declare module '*.jpg' {
+  const img: string
+
+  export default img
+}
+
+declare module '*.jpeg' {
+  const img: string
+
+  export default img
+}
