07cce39478eda809cd3da35429079ec4fc9fd632
TShokhrukh <shohruh8945@gmail.com>
2023-08-13 15:51:10 +0500

    update ProjectCard

---
+++ src/components/ProjectCard/Img/ProjectCard.-Img.tsx
@@ -0,0 +1,15 @@
+import React, { FC, useContext } from 'react'
+import { Img } from '~/components/Img/build'
+import { Context, cnProjectCard } from '../ProjectCard.const'
+import type { IProjectCardContext } from '../__types__'
+import './ProjectCard-Img.scss'
+
+export const ProjectCardImg: FC = () => {
+  const { project } = useContext<IProjectCardContext>(Context)
+
+  return (
+    <div className={cnProjectCard('Image', [])}>
+      <Img alt={project.name} src={project.image} fill='horizontal' />
+    </div>
+  )
+}
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -2,10 +2,10 @@ import React, { FC } from 'react'
 import { useTranslator } from '@eo-locale/react'
 import { Card } from '~/components/Card/Card'
 import { Link } from '~/components/Link/Link'
-import { Img } from '~/components/Img/Img'
 import { Typography } from '~/components/Typography/build/base'
 import { Context, cnProjectCard } from './ProjectCard.const'
 import { ProjectCardStacks } from './Stacks/ProjectCard-Stacks'
+import { ProjectCardImg } from './Img'
 import type { IProjectCardProps } from './__types__'
 import './ProjectCard.scss'
 
@@ -20,7 +20,7 @@ export const ProjectCard: FC<IProjectCardProps> = ({
         <Typography color='white'>{project.name}</Typography>
         <Typography className={cnProjectCard('Desc', [])}>{project.desc[t.language]}</Typography>
         <ProjectCardStacks />
-        <Img className={cnProjectCard('Image', [])} alt={project.name} src={project.image} />
+        <ProjectCardImg />
       </Card>
     </Context.Provider>
   )
