63c5835de2e3380e329a8042adb1a83ac9dd992d
TShokhrukh <shohruh8945@gmail.com>
2023-08-17 16:35:54 +0500

    update Button variant link

--- src/components/Button/__types__/Button_variant_link.types.ts
+++ src/components/Button/__types__/Button_variant_link.types.ts
@@ -1,5 +1,7 @@
 export interface IButtonVariantLinkProps {
   variant?: 'link',
-  type?: undefined,
-  href?: string
+  type?: null,
+  href?: string,
+  rel?: string,
+  target?: '_blank'|'_parent'|'_self'|'_top'
 }
