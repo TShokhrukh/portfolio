ea91f7ae9b4542b4bfd0207622b4a9ff1e8f973a
TShokhrukh <shohruh8945@gmail.com>
2023-08-27 16:29:57 +0500

    static folder added

---
+++ src/static/data/careers.ts
@@ -0,0 +1,15 @@
+import type { TCareersList } from '~/types'
+
+export const CAREERS: TCareersList = [
+  {
+    id: '1',
+    active: false,
+    title: 'TECHNOUNIT-GROUP',
+    desc: {
+      en: 'Development and maintenance of web interfaces, applications and blocks for Bitrix24.',
+      ru: 'Разработка и поддержка веб-интерфейсов, приложений и блоков для Bitrix24.'
+    },
+    startDate: '2020-07-01',
+    endDate: '2023-05-01'
+  }
+]
