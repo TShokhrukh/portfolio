747eee61590260b80d3c3b9ff8ac172737db77ce
TShokhrukh <shohruh8945@gmail.com>
2023-08-27 14:57:56 +0500

    typo fix

--- src/types/global/process.d.ts
+++ src/types/global/process.d.ts
@@ -2,7 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DEFAULT_LANGUAGE: string,
-      SUPPORTED_LANGUAGE: string,
+      SUPPORTED_LANGUAGES: string,
       NODE_ENV: 'development'|'production',
       CONTACT_GITHUB: string,
       CONTACT_TELEGRAM: string,
