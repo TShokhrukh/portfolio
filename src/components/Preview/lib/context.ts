bc45af1a772bbffbafe60ef8f189adc1a06d1306
TShokhrukh <shohruh8945@gmail.com>
2023-09-02 15:19:08 +0500

    the PreviewContext has been refactored

--- src/components/Preview/lib/context.ts
+++ src/components/Preview/lib/context.ts
@@ -4,5 +4,9 @@ import type { IPreviewContext } from '../__types__'
 export const Context = createContext<IPreviewContext>(null as any)
 
 export const usePreviewContext = (): IPreviewContext => {
-  return useContext<IPreviewContext>(Context)
+  const c = useContext<IPreviewContext>(Context)
+  if (!c) {
+    throw new Error('PreviewContext has not been set')
+  }
+  return c
 }
