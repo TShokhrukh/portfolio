a1dc8356615a1138497662a3f9f090c63c5c4fe9
TShokhrukh <shohruh8945@gmail.com>
2023-09-02 16:04:56 +0500

    projects have been updated

--- src/static/data/projects.ts
+++ src/static/data/projects.ts
@@ -1,5 +1,7 @@
 import vmajlis from '~/static/img/vmajlis.png'
 import itpu from '~/static/img/itpu.png'
+import bitrix24 from '~/static/img/bitrix24.png'
+import didox24 from '~/static/img/didox24.png'
 import type { TProjectsList } from '~/types'
 
 export const PROJECTS: TProjectsList = [
@@ -8,32 +10,43 @@ export const PROJECTS: TProjectsList = [
     name: 'vMajlis',
     href: '#',
     desc: {
-      en: 'Develop the interface for a video communication system',
-      ru: 'Разработал интерфейса для системы видео связи'
+      en: 'Development of a user interface based on a template and interaction with the backend.',
+      ru: 'Разработка пользовательского интерфейса по шаблону и взаимодействие с бэкендом.'
     },
     stack: ['JavaScript'],
     image: vmajlis
   },
   {
     id: '2',
-    name: 'ITPU',
-    href: 'https://itpu.uz',
+    name: 'Bitrix24 Blocks',
+    href: '№',
     desc: {
-      en: 'Develop some custom blocks for Bitrix24',
-      ru: 'Разработал некоторые пользовательские блоки для Bitrix24'
+      en: 'Development of custom blocks for the Bitrix24.',
+      ru: 'Разработка пользовательских блоков для Bitrix24.'
     },
-    stack: ['JavaScript', 'Bitrix24'],
-    image: itpu
+    stack: ['JavaScript', 'PHP'],
+    image: bitrix24
   },
   {
     id: '3',
     name: 'Admission Portal',
     href: 'https://my.itpu.uz/',
     desc: {
-      en: 'Develop the portal interface for students',
-      ru: 'Разработал интерфейс портала для студентов'
+      en: 'Development of the user interface based on a layout and interaction with the backend.',
+      ru: 'Разработка пользовательского интерфейса по макету и взаимодействие с бэкендом.'
     },
     stack: ['JavaScript', 'React'],
     image: itpu
+  },
+  {
+    id: '4',
+    name: 'Didox24',
+    href: 'https://www.bitrix24.ru/apps/app/tug.didox/',
+    desc: {
+      en: 'Didox24 application Development',
+      ru: 'Разработка приложения Didox24'
+    },
+    stack: ['JavaScript', 'React'],
+    image: didox24
   }
 ]
