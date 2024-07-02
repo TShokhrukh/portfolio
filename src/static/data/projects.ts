ff7baeac0fa42178f3af2d6de56b6542a546e0cb
TShokhruh <shohruh8945@gmail.com>
2024-07-02 14:33:52 +0500

    update static data

--- src/static/data/projects.ts
+++ src/static/data/projects.ts
@@ -1,21 +1,9 @@
-import vmajlis from '~/static/img/vmajlis.png'
 import itpu from '~/static/img/itpu.png'
 import type { TProjectsList } from '~/types'
 
 export const PROJECTS: TProjectsList = [
   {
     id: '1',
-    name: 'vMajlis',
-    href: '#',
-    desc: {
-      en: 'Development of a user interface based on a template and interaction with the backend.',
-      ru: 'Разработка пользовательского интерфейса по шаблону и взаимодействие с бэкендом.'
-    },
-    stack: ['JavaScript'],
-    image: vmajlis
-  },
-  {
-    id: '2',
     name: 'Admission Portal',
     href: 'https://my.itpu.uz/',
     desc: {
